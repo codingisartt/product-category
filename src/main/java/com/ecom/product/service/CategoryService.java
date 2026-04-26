728c2116efa3414f3377601d666444469de0799e
Tubanur Balci <tubanurbalci5015@gmail.com>
2026-04-26 14:50:42 +0300

    refactor: make service dependencies final for immutability

--- src/main/java/com/ecom/product/service/CategoryService.java
+++ src/main/java/com/ecom/product/service/CategoryService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 public class CategoryService {
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
     public CategoryService (CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
